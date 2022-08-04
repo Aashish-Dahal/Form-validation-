0b696f13c5912a418d8187589752de2b9c73849c
Aashish-Dahal <ashishdahal490@gmail.com>
2022-08-04 17:58:44 +0545

    feat: Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+export const Home = () => {
+  return (
+    <View>
+      <Text style={{color: 'black'}}>Home Screen</Text>
+    </View>
+  );
+  
+}
