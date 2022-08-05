248900107325d9986de6b2cee70d9df669146542
Aashish-Dahal <ashishdahal490@gmail.com>
2022-08-05 17:52:38 +0545

    added logout fuctionality

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,26 @@
-
-import React from "react"
-import { StyleSheet, View, Text } from "react-native"
+import React, {useContext} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {Button} from '../../components/atoms/Button';
+import {AuthContext} from '../../lib/auth/AuthContext';
 export const Home = () => {
+  const {logout} = useContext(AuthContext);
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={{color: 'black'}}>Home Screen</Text>
+      <View style={styles.button}>
+        <Button onPress={() => logout()} title="Logout" />
+      </View>
     </View>
   );
-  
-}
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    marginTop: 20,
+    width: '50%',
+  },
+});
